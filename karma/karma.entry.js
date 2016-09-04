85f49d7de7c8f844d550dc5688f56a0f40b979e2
Matt Fehskens <matt.fehskens@gmail.com>
2016-09-04 09:35:18 -0400

    Updated to RC6

--- karma/karma.entry.js
+++ karma/karma.entry.js
@@ -2,10 +2,11 @@ require('es6-shim');
 require('reflect-metadata');
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 require('zone.js/dist/sync-test');
+require('zone.js/dist/proxy');
+require('zone.js/dist/jasmine-patch');
 
 const browserTesting = require('@angular/platform-browser-dynamic/testing');
 const coreTesting = require('@angular/core/testing');
@@ -14,9 +15,10 @@ const context = require.context('../src/', true, /\.spec\.ts$/);
 Error.stackTraceLimit = Infinity;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
 
-coreTesting.setBaseTestProviders(
-    browserTesting.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-    browserTesting.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
+coreTesting.TestBed.resetTestEnvironment();
+coreTesting.TestBed.initTestEnvironment(
+    browserTesting.BrowserDynamicTestingModule,
+    browserTesting.platformBrowserDynamicTesting()
 );
 
 context.keys().forEach(context);
